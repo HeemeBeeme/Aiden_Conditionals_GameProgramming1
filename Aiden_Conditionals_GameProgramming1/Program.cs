f5c9f74201656342b58398aee21cdc8eacd0a4c7
Heeme Beeme <heemebeeme@gmail.com>
2025-09-15 12:17:37 -0300

    Potions/Healing, Randomness, and more hud modifications

--- Aiden_Conditionals_GameProgramming1/Program.cs
+++ Aiden_Conditionals_GameProgramming1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Security;
 using System.Text;
@@ -11,20 +12,32 @@ namespace Aiden_Conditionals_GameProgramming1
     {
         enum WeaponType
         {
-            Grenade,
             Fist,
+            Grenade,
             Knife,
             Pistol,
-            Shotgun,
+            Shotgun
         }
 
         static string weaponName;
-        static string Status;
+        static WeaponType weapon = WeaponType.Fist;
 
+        static string Status;
+        static int CapableDamage;
         static int MaxHealth = 100;
         static int Health = 100;
+        static int TakenDmG;
+        static int PotionPotency;
+        static string PotionName;
 
-        static string HealthHUD = $"{Health}/{MaxHealth}";
+        enum PotionType
+        {
+            Weak_Potion,
+            Moderate_Potion,
+            Strong_Potion
+        }
+        static PotionType potion = PotionType.Weak_Potion;
+
         static string HealthStatus()
         {
 
@@ -53,50 +66,175 @@ namespace Aiden_Conditionals_GameProgramming1
                 return Status;
         }
 
+        static void HealText()
+        {
+            Console.WriteLine($"You heal yourself with a {PotionName} for {PotionPotency} Health!");
+        }
+
+        static void SkipLine()
+        {
+            Console.WriteLine();
+        }
         static void showHUD()
         {
             HealthStatus();
-
-            Console.WriteLine("{0,0}{1,30}{2,15}", $"Health: {HealthHUD}", $"Status: {Status}", $"Weapon: {weaponName}");
-        }
-
-        static void Main(string[] args)
-        {
-
-            WeaponType weapon = WeaponType.Fist;
 
             switch (weapon)
             {
                 case WeaponType.Grenade:
                     weaponName = "Grenade";
+                    CapableDamage = DamageRnD.Next(10, 40);
                     break;
 
                 case WeaponType.Fist:
                     weaponName = "Fist";
+                    CapableDamage = DamageRnD.Next(2, 5);
                     break;
 
                 case WeaponType.Knife:
                     weaponName = "Knife";
+                    CapableDamage = DamageRnD.Next(3, 10);
                     break;
 
                 case WeaponType.Pistol:
                     weaponName = "Pistol";
+                    CapableDamage = DamageRnD.Next(7, 20);
                     break;
 
                 case WeaponType.Shotgun:
                     weaponName = "Shotgun";
+                    CapableDamage = DamageRnD.Next(20, 35);
                     break;
             }
 
-            Health -= 10;
-            Console.WriteLine($"{Health}");
+            Console.WriteLine("{0,0}{1,30}{2,20}", $"Health: {Health}/{MaxHealth}", $"Status: {Status}", $"Weapon: {weaponName}");
+        }
+
+        static Random DamageRnD = new Random();
+        static Random WeaponRnD = new Random();
+        static Random TakenDmgRnD = new Random();
+        static Random PotionRnD = new Random();
+
+
+        static void RandomWeapon()
+        {
+            int WeaponNumb = WeaponRnD.Next(1, 4);
+            weapon = (WeaponType)WeaponNumb;
+        }
+
+        static void RandomPotion()
+        {
+            int PotionNumb = PotionRnD.Next(0,2);
+            potion = (PotionType)PotionNumb;
+
+            switch (potion)
+            {
+                case PotionType.Weak_Potion:
+                    PotionName = "Weak Potion";
+                    PotionPotency = 10;
+                    break;
+                case PotionType.Moderate_Potion:
+                    PotionName = "Moderate Potion";
+                    PotionPotency = 25;
+                    break;
+                case PotionType.Strong_Potion:
+                    PotionName = "Strong Potion";
+                    PotionPotency = 50;
+                    break;
+            }
+
+            Health += PotionPotency;
+
+        }
+
+        static void DamageDealt()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Damage Dealt: {CapableDamage}");
+            Console.ResetColor();
+        }
+
+        static void DamageTaken()
+        {
+            TakenDmG = TakenDmgRnD.Next(6, 25);
+            Health -= TakenDmG;
+        }
+
+        static void DamageTakenText()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Damage Taken: {TakenDmG}");
+            Console.ResetColor();
+        }
+
+        static void Main(string[] args)
+        {
+
             showHUD();
-            Console.WriteLine();
-            Console.WriteLine("You are walking in the forest and you step on some thorns!");
-
+            SkipLine();
+            Console.WriteLine("You are walking in the forest...");
+            SkipLine();
             Console.ReadKey();
+            Console.Clear();
 
-            
+            Health -= 10;
+            showHUD();
+            SkipLine();
+            Console.WriteLine("Ahh! You step into some thorns!");
+            SkipLine();
+            Console.ReadKey();
+            Console.Clear();
+
+            showHUD();
+            SkipLine();
+            Console.WriteLine("You finish picking thorns out of your cloak as you enter a cave...");
+            SkipLine();
+            Console.ReadKey();
+            Console.Clear();
+
+            showHUD();
+            SkipLine();
+            Console.WriteLine("The Cave is damp and dark, but you see something on the ground...");
+            SkipLine();
+            Console.ReadKey();
+            Console.Clear();
+
+            RandomWeapon();
+            showHUD();
+            SkipLine();
+            Console.WriteLine($"You found a {weaponName}!");
+            SkipLine();
+            Console.ReadKey();
+            Console.Clear();
+
+            showHUD();
+            SkipLine();
+            Console.WriteLine("A goblin runs out after you, you act quickly and attack it!");
+            SkipLine();
+            DamageDealt();
+            Console.ReadKey();
+            Console.Clear();
+
+
+            DamageTaken();
+            showHUD();
+            SkipLine();
+            Console.WriteLine("The goblin hits you with his club!");
+            SkipLine();
+            DamageTakenText();
+            Console.ReadKey();
+            Console.Clear();
+
+
+            RandomPotion();
+            showHUD();
+            SkipLine();
+            HealText();
+            SkipLine();
+            Console.ReadKey();
+            Console.Clear();
+
+
         }
     }
 }
