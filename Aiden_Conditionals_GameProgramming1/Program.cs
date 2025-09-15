37bd5a586b96e81d9a2b9fc82aa58c805fa494c6
Heeme Beeme <heemebeeme@gmail.com>
2025-09-15 09:29:51 -0300

    Initial commit

---
+++ Aiden_Conditionals_GameProgramming1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aiden_Conditionals_GameProgramming1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+           
+
+            
+        }
+    }
+}
