eda176ba9b2ca285db5b0fc508617cec3fef016d
Heeme Beeme <heemebeeme@gmail.com>
2025-09-15 09:58:07 -0300

    enum weapontype, health, basic hud

--- Aiden_Conditionals_GameProgramming1/Program.cs
+++ Aiden_Conditionals_GameProgramming1/Program.cs
@@ -8,10 +8,55 @@ namespace Aiden_Conditionals_GameProgramming1
 {
     internal class Program
     {
+        enum WeaponType
+        {
+            Grenade,
+            Fist,
+            Knife,
+            Pistol,
+            Shotgun,
+        }
+
+
+        static int MaxHealth = 100;
+        static int Health = 100;
+
+        static string HealthHUD = $"{Health}/{MaxHealth}";
+
+        static void showHUD()
+        {
+            Console.WriteLine("{0,0}{0,0}{0,0}", $"Health: {HealthHUD}");
+        }
 
         static void Main(string[] args)
         {
-           
+
+            WeaponType weapon = WeaponType.Fist;
+
+            switch (weapon)
+            {
+                case WeaponType.Grenade:
+                    //something
+                    break;
+
+                case WeaponType.Fist:
+                    //something
+                    break;
+
+                case WeaponType.Knife:
+                    //something
+                    break;
+
+                case WeaponType.Pistol:
+                    //something
+                    break;
+
+                case WeaponType.Shotgun:
+                    //something
+                    break;
+            }
+
+            showHUD();
 
             
         }
